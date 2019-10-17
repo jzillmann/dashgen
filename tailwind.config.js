ef3d69de16f0d2dffc6d65495f29d72b49618ecb
Johannes Zillmann <jz@datameer.com>
2019-10-17 22:16:04 +0200

    Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {},
+            textColor: {},
+            backgroundColor: {},
+        }
+    },
+    variants: {},
+    plugins: []
+};
