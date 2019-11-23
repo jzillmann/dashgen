58eaefca447f52aafc3ee9d261b546b33bf9849a
Johannes Zillmann <jz@datameer.com>
2019-11-23 22:16:51 +0100

    Hover effect for totals

--- src/components/Totals.svelte
+++ src/components/Totals.svelte
@@ -7,14 +7,23 @@
 </script>
 
 <div class="absolute" transition:fade>
-  <div class="shadow-lg">
+  <div class="flex">
     <div
-      class="p-2 text-center font-extrabold text-2xl text-gray-700 bg-gray-300"
+      class="group m-4 cursor-default rounded bg-gray-300 shadow-lg hover:shadow-xl bg-gray-100 hover:scale-1"
+      style="transition: all .9s;"
     >
-      {sourceCount}
-    </div>
-    <div class="p-1 text-center font-hairline text-gray-800 bg-gray-400">
-      Sources
+      <div
+        class="m-1 p-2 text-center rounded-full font-semibold text-2xl text-gray-700 bg-gray-100 group-hover:bg-gray-300"
+        style="transition: all .9s;"
+      >
+        {sourceCount}
+      </div>
+      <div
+        class="p-1 text-center rounded-b font-hairline text-gray-800 bg-gray-300 group-hover:up-7 group-hover:text-gray-900 group-hover:bg-gray-400"
+        style="transition: all .9s;"
+      >
+        Sources
+      </div>
     </div>
   </div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-    theme: {
-        extend: {
-            colors: {},
-            textColor: {},
-            backgroundColor: {},
-        }
-    },
-    variants: {},
-    plugins: []
+  theme: {
+    extend: {
+      colors: {},
+      textColor: {},
+      backgroundColor: {}
+    }
+  },
+  variants: {
+    backgroundColor: ["responsive", "hover", "focus", "group-hover"],
+    textColor: ["responsive", "hover", "focus", "group-hover"]
+  },
+  plugins: []
 };
